300853b99b5af6d83608c71f28791e8c779f61bd
mollo <mollo@PCMOIKAS>
2023-10-29 20:34:28 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 10 - Suma Recursiva e Iterativa ITERATIVA EN PITIN/Actividad_10___Suma_Recursiva_e_Iterativa_ITERATIVA_EN_PITIN.py
@@ -0,0 +1,17 @@
+def sumaIterativa(n):
+    suma = 0
+
+    while n > 9:
+        suma += n % 10
+        n //= 10
+
+    return suma + n
+
+def main():
+    numero = 12345
+    resultado = sumaIterativa(numero)
+    print(f"La suma de los dígitos de {numero} es {resultado}")
+
+if __name__ == "__main__":
+    main()
+
